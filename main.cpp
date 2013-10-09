f1b9b6633f8357b62a9eadc59a1951901092dac5
diogoalfa <Diogo@client-186-37-30-84.imovil.entelpcs.cl>
2013-10-08 22:06:43 -0300

    Tarea N1

---
+++ main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  TareaN1Software
+//
+//  Created by Diego Cristobal Navia Fuentes on 08-10-13.
+//  Copyright (c) 2013 Diego Cristobal Navia Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    string opcionString;
+    int opcion = 0;
+
+    cout<<"OPCIONES : "<<endl;
+    cout<<"-g = INFORMACION CREADORES "<<endl;
+    cout<<"::INGRESE UNA OPCION::"<<endl;
+    cin>>opcionString;
+    
+    if (opcionString.compare("-g")) {
+        opcion=1;
+    }
+    
+    
+    switch (opcion) {
+        case 1:
+            cout<<"hola"<<endl;
+            break;
+            
+        default:
+            break;
+    }
+    
+    
+    
+    return 0;
+}
+
