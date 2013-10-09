5396fb8a022686142206941e47708d0478748829
diogoalfa <Diogo@MacBook-Pro-de-Diego.local>
2013-10-09 15:49:43 -0300

    Menu del Programa
    Este es el menu del programa

--- main.cpp
+++ main.cpp
@@ -16,12 +16,28 @@ int main(int argc, const char * argv[])
     int opcion = 0;
 
     cout<<"OPCIONES : "<<endl;
+    cout<<"-i = INTEGRAL DEL UN POLINOMIO "<<endl;
+    cout<<"-v = GRAFICA BAJO DE CURVA DEL POLINOMIO "<<endl;
     cout<<"-g = INFORMACION CREADORES "<<endl;
-    cout<<"::INGRESE UNA OPCION::"<<endl;
+    cout<<"::INGRESE UNA OPCION::"<<endl<<endl;
     cin>>opcionString;
     
-    if (opcionString=="-g") {
+    if (opcionString=="-i") {
         opcion=1;
+    }
+    else{
+        
+        if (opcionString=="-v") {
+            opcion=2;
+        }
+        else{
+            if (opcionString=="-g") {
+                opcion=3;
+            }
+            else{
+                opcion=4;
+            }
+        }
     }
     
     
@@ -29,7 +45,13 @@ int main(int argc, const char * argv[])
         case 1:
             cout<<"hola"<<endl;
             break;
-            
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            cout<<"INGRESE OPCION CORRECTA"<<endl;
+            break;
         default:
             break;
     }
