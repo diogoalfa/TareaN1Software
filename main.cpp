6033249cd969fb526e2389ddec1efc3fa09bbec0
faxtork <sebastian.esparza@ceinf.cl>
2013-10-10 20:35:52 -0300

    integrado
    lo actualize con el tema del argv
    para correrlo es
    g++ main.cpp   [ENTER]
    a.exe -i x^2  (en caso de windows) o ./a.out -i x^2 (caso de linux)
    *prueba en mac navia

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 //
-//  main.cpp
-//  TareaN1Software
+// main.cpp
+// TareaN1Software
 //
-//  Created by Diego Cristobal Navia Fuentes on 08-10-13.
-//  Copyright (c) 2013 Diego Cristobal Navia Fuentes. All rights reserved.
+// Created by Diego Cristobal Navia Fuentes on 08-10-13.
+// Copyright (c) 2013 Diego Cristobal Navia Fuentes. All rights reserved.
 //
 
 #include <iostream>
@@ -97,7 +97,7 @@ void integrarConstante(vectorChar polinomios[],int numPolinomio){
     int i=numPolinomio;
     
     largoPolinomio=polinomios[i].largo;
-  //  cout<<"Largo POl:"<<largoPolinomio<<endl;
+  // cout<<"Largo POl:"<<largoPolinomio<<endl;
     
      polinomios[i].polinomio[largoPolinomio]='x';
 
@@ -197,63 +197,44 @@ int main(int argc, const char * argv[])
     int opcion = 0;
     char polinomio[100];
     int opcionDoWhile=0;
-do {
-    
-    cout<<"OPCIONES : "<<endl;
-    cout<<"-i = INTEGRAL DEL UN POLINOMIO "<<endl;
-    cout<<"-g = GRAFICA BAJO DE CURVA DEL POLINOMIO "<<endl;
-    cout<<"-v = INFORMACION CREADORES "<<endl;
-    cout<<"::INGRESE UNA OPCION::"<<endl<<endl;
-    
-    cin>>opcionString;
-    
-    if (opcionString=="-i") {
-        opcion=1;
+    int esta=0;
+   for (int i = 0; i < argc ; i++ ) 
+   {
+        if (strcmp(argv[i],"-i")==0) { //si es 0 es por que son iguales
+            opcion=1;
+            
+        }  
+        if(strcmp(argv[i],"-v")==0) {
+                opcion=2;
+              
+            }
+         if(strcmp(argv[i],"-g")==0) {
+                    opcion=3;
+                    
+                }       
     }
-    else{
-        
-        if (opcionString=="-v") {
-            opcion=2;
-        }
-        else{
-            if (opcionString=="-g") {
-                opcion=3;
-            }
-            else{
-                opcion=4;
-            }
-        }
+    if (opcion==0)//si no cambio la opcion se le asigna 4
+    {
+       opcion=4;
     }
     
+        strcpy( polinomio, argv[2] );
     
         switch (opcion) {
             case 1:
-                
-                cout<<"Ingrese un polinomio"<<endl;
-                cin>>polinomio;
                 cout<<endl;
-                descomponerPolinomio(polinomio);
+                descomponerPolinomio(polinomio);          
                 break;
             case 2:
+                cout<<"Datos integrantes"<<endl;
                 break;
             case 3:
-                break;
+                 cout<<"PNG"<<endl;
+                 break;
             case 4:
                 cout<<"INGRESE OPCION CORRECTA"<<endl;
-                opcionDoWhile=0;
                 break;
-            case 5:
-                cout<<"ADIOS :D "<<endl;
-                opcionDoWhile=1;
-                
-            default:
-                break;
-        }
-} while (opcionDoWhile==0);
-    
-    
-    
+                        }
     
     return 0;
 }
-
